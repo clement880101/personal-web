29c4be2b6b27b5e6433f74809df0b3a5b7d547c1
Clement Chang <clement880101@gmail.com>
2022-07-07 20:41:22 -0700

    firebase

---
+++ src/api/firebaseConfig.js
@@ -0,0 +1,17 @@
+import {initializeApp} from 'firebase/app'
+import {getFirestore} from 'firebase/firestore'
+const firebaseConfig = {
+    apiKey: "AIzaSyA8O4Etd9qQANBJzz_otSqCEEHGAiU9mkg",
+    authDomain: "personalwebsite-4b72f.firebaseapp.com",
+    projectId: "personalwebsite-4b72f",
+    storageBucket: "personalwebsite-4b72f.appspot.com",
+    messagingSenderId: "824149975722",
+    appId: "1:824149975722:web:f7cfabafc00f9bcd5766d3",
+    measurementId: "G-HJ8H8JDLP3"
+};
+
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+
+
+export {app, db};
