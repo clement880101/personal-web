b55744ddfefdb8612323f7ec89d7487caa03ba81
Clement Chang <clement880101@gmail.com>
2022-07-10 01:39:21 -0700

    lol

--- src/components/Skillcard.js
+++ src/components/Skillcard.js
@@ -1,10 +1,12 @@
 import { Card, CardContent, Typography } from "@mui/material"
 
-export default function SkillCard(){
+export default function SkillCard({title}){
     return(
         <Card sx={{width:300, height:400, borderRadius:4}}>
             <CardContent>
-                <Typography>Hello</Typography>
+                <Typography>{title}</Typography>
+                <Typography>I know these languages</Typography>
+                <Typography>Things that I am skilled in</Typography>
             </CardContent>
         </Card>
     )
--- src/components/Skillmodule.js
+++ src/components/Skillmodule.js
@@ -1,7 +1,39 @@
+import { Box, ButtonGroup, Button} from "@mui/material"
+import { useState } from "react"
 import SkillCard from "./SkillCard"
 
 export default function Skillmodule() {
+    const [selected, setSelected] = useState("DevOps")
+
     return(
-        <SkillCard/>
+        <Box sx={{width:"100%", display:"flex", flexDirection:"column", alignItems:"center"}}>
+            <ButtonGroup size="small" sx={{ alignSelf: "center", margin:1}} disableElevation disableRipple>
+                <Button variant={selected === "DevOps" ? "contained" : "outlined"} 
+                onClick={() => setSelected("DevOps")}>
+                    DevOps
+                </Button>
+                <Button variant={selected === "Frontend" ? "contained" : "outlined"} 
+                onClick={() => setSelected("Frontend")}>
+                    Frontend
+                </Button>
+                <Button variant={selected === "Backend" ? "contained" : "outlined"} 
+                onClick={() => setSelected("Backend")}>
+                    Backend
+                </Button>
+                <Button variant={selected === "Blockchain" ? "contained" : "outlined"} 
+                onClick={() => setSelected("Blockchain")}>
+                    Blockchain
+                </Button>
+                <Button variant={selected === "Life" ? "contained" : "outlined"} 
+                onClick={() => setSelected("Life")}>
+                    Life
+                </Button>
+            </ButtonGroup>
+            <Box sx={{width:"100%", display:"flex", flexDirection:"row", flexWrap:"wrap", 
+            justifyContent:"center", gap:2}}>
+                <SkillCard title={selected}/>
+                <SkillCard title={selected}/>
+            </Box>
+        </Box>
     )
 }
